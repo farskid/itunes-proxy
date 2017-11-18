cb2c5a827db375bd57982a99e080a64282565b83
MohammadHossein Abedinpour <abedinpourmh@gmail.com>
2017-11-19 01:53:07 +0330

    Update cache.js

--- server/cache.js
+++ server/cache.js
@@ -1,7 +1,9 @@
 const redis = require("redis");
 
+// TODO: Read redis connection string from config
 const client = redis.createClient();
 
+// TODO: Better error handling
 client.on("error", err => {
   console.error(err);
 });
